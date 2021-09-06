5509f2408992488b904e81b8c60b2a8766fdd7eb
Lucas Plissonnier <lucas@twic.pics>
2021-09-06 14:49:32 +0200

    Remove unnecessary window.onload and twic:debug value

--- chrome/content.js
+++ chrome/content.js
@@ -1,24 +1,21 @@
-chrome.storage.onChanged.addListener(function (data) {
-    var isDebugActive = data['debugMode'].newValue;
-    var root = document.documentElement;
-    console.log(`In storage changed listener : isDebugActive = ${isDebugActive}`);
+chrome.storage.onChanged.addListener(function (changes) {
+    if ('debugMode' in changes) {
+        var isDebugActive = changes['debugMode'].newValue;
+        var root = document.documentElement;
+        console.log(`In storage changed listener : isDebugActive = ${isDebugActive}`);
 
-    if (isDebugActive === true) {
-        root.setAttribute("twic:debug", "");
-    } else {
-        root.removeAttribute("twic:debug");
+        if (isDebugActive === true) {
+            root.setAttribute("twic:debug", "");
+        } else {
+            root.removeAttribute("twic:debug");
+        }
     }
 });
 
-window.onload = function () {
-    chrome.storage.sync.get('debugMode', function (data) {
-        let isDebugActive = data.debugMode;
-        if (isDebugActive === true) {
-            var root = document.documentElement;
-            root.setAttribute("twic:debug", "true");
-        }
-    });
-}
-
-
-
+chrome.storage.sync.get('debugMode', function (data) {
+    let isDebugActive = data.debugMode;
+    if (isDebugActive === true) {
+        var root = document.documentElement;
+        root.setAttribute("twic:debug", "");
+    }
+});
